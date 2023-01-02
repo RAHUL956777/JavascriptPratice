c763c0789a5d244ebaf3429ccc1952a3650f2e49
RAHUL <dasr16583@gmail.com>
2023-01-02 20:12:06 +0530

    For loop added

--- 09_forLoop.js
+++ 09_forLoop.js
@@ -15,3 +15,18 @@ for (let i = n; i >= 1; i--) {
 }
 
 console.log('sum:', sum);
+
+/* program to generate a multiplication table
+upto a range */
+
+// take number input from the user
+const number = parseInt(prompt('Enter an integer: '));
+
+// take range input from the user
+const range = parseInt(prompt('Enter a range: '));
+
+//creating a multiplication table
+for (let i = 1; i <= range; i++) {
+    const result = i * number;
+    console.log(`${number} * ${i} = ${result}`);
+}
