036861ac13512c3e48458f9e764000f16dd306f5
RAHUL <dasr16583@gmail.com>
2023-01-02 20:45:37 +0530

    While loop and do while loop is added

---
+++ 10_whileloop.js
@@ -0,0 +1,20 @@
+// while loop
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n);
+
+let i = 0;
+while (i < n) {
+    console.log(i);
+    i++;
+}
+
+// do while loop
+let nu = prompt("Enter the value of n");
+nu = Number.parseInt(n);
+
+let j = 0;
+do{
+    console.log(i);
+    i++;
+}
+while (j < n)
