57e06d858eae3b6c7ef693607a147ea5d6cb1048
RAHUL <dasr16583@gmail.com>
2023-01-04 11:02:53 +0530

    Working on string

---
+++ 13_string.js
@@ -0,0 +1,20 @@
+// string is a collection of character
+let na = "Rahul";
+console.log(na.length);
+console.log(na[0]);
+console.log(na[1]);
+console.log(na[2]);
+console.log(na[3]);
+console.log(na[4]);
+
+let friend = "Sushanta";
+console.log(friend);
+
+// template litilers
+let boy1 = "Rahul";
+let boy2 = "Ajay";
+let Friends = `${boy1} is a friend of ${boy2}`;
+
+// escape sequence 
+let fruit = 'Bana\'na'; // you can use also " in the place of ' code....
+console.log(fruit);
